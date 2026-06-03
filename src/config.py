de156a0cf90f635d2d31fab54dab19d11d72a60f
nicolaspichon-git <nicolaspichon@protonmail.com>
2026-06-03 18:22:12 +0200

    np : work on utils (requesting all records as json)

--- src/config.py
+++ src/config.py
@@ -1,2 +1,6 @@
 # Sites de mesures hydrométries pour l'entraînement du modèle de prévision de risque d'inondation
-TIME_PERIOD = ("2007-01-01", "2022-12-31")
+import os
+LOCAL_DATA_DIRECTORY = os.path.abspath(os.path.join("data", "hubeau"))
+
+TRAINING_TIME_PERIOD = ("2007-01-01", "2025-12-31")
+TESTING_TIME_PERIOD = ("2026-01-01", "2026-06-01")
--- src/utils.py
+++ src/utils.py
@@ -8,65 +8,90 @@ from urllib.parse import urlparse, parse_qs
 #  UTILITAIRES
 # ============================================================
 
-def request_json_data(url, params, timeout=60): # return requested json data or raise exception
+def request_json(url, params, timeout=60): # return requested json data or raise exception
     """
-    Execute HTTP GET request and return data as JSON or raise an exception.
+    Execute the given API point as HTTP GET request with requested JSON output and return JSON output data or raise an exception.
     """
     r = requests.get(url, params=params, timeout=timeout, headers={"Accept": "application/json"})
     r.raise_for_status()
-    return r.json()
+    output = r.json()  # content as json object (dictionary)
+    return output
 
-def request_with_pagination(url, params, pages_delay_in_seconds=0.3): # return list of all results from paginated endpoint
+def request_json_all(url, params, next_pages_delay_in_seconds = 0.3, timeout_per_page = 60, verbose = False): 
     """
-    Parcourt toutes les pages d'un endpoint paginé Hub'Eau.
+    Parcourt toutes les pages d'un endpoint paginé u.
     Retourne la liste complète des résultats.
     """
-    results = []
+
+    assert(isinstance(params, dict))
+
+    api_version = None
+    count = None
+    data = []
+    
     cursor = None
     page_number = 1
+    total_records = 0
 
-    while True:  # break if cursor is None or if no more results
-        pp = dict(params)
+    done = False  # break if cursor is None or if no more results
+    while not done:
+        done = True
+
+        pp = dict(**params)
         if cursor:
             pp["cursor"] = cursor
 
-        data_page_as_json = request_json_data(url, params=pp)
-        if not data_page_as_json or "data" not in data_page_as_json:
-            break
+        json_page = request_json(url, params=pp, timeout=timeout_per_page)
+        if json_page:
+            assert(isinstance(json_page, dict))
+            assert(all(key in json_page for key in ("api_version", "count", "data", "next")))
 
-        result = data_page_as_json.get("data", [])
-        results.extend(result)
-        print(f"    page {page_number} → {len(result)} enregistrements (total : {len(results)})")
+            page_api_version = json_page["api_version"]
+            if api_version is None:
+                api_version = page_api_version
+            assert(page_api_version == api_version)
+            
+            page_count = json_page["count"]
+            if count is None:
+                count = page_count
+            assert(page_count == count)
 
-        # Récupérer le curseur de pagination
-        next_url = data_page_as_json.get("next")
-        if not next_url or not result:
-            break
+            page_data = json_page.get("data", [])
+            data.extend(page_data)
 
-        qs = parse_qs(urlparse(next_url).query)
-        cursor = qs.get("cursor", [None])[0]
-        if not cursor:
-            break
+            # info:
+            local_records = len(page_data)
+            total_records += local_records
+            if verbose:
+                print(f">>> page {page_number} -> {local_records} enregistrements / {total_records}")
 
-        # continue :
+            next_url = json_page.get("next")
+            if next_url:
+                qs = parse_qs(urlparse(next_url).query)
+                cursor = qs.get("cursor", [None])[0]
+                if cursor:
+                    done = False  # continue with next page
 
-        page_number += 1
-        time.sleep(pages_delay_in_seconds) # respecter les limites de l'API (~10 req/s)
+                    # info:
+                    page_number += 1
 
-    return results
+                     # respecter les limites de l'API (0.3s ~> 10 req/s) 
+                    if next_pages_delay_in_seconds > 0:
+                        time.sleep(next_pages_delay_in_seconds)      
 
-def save_dataframe_as_csv(df, nom, type_donnee, output_dir, verbose=False) -> str:
+    return {"api_version": api_version, "count": count, "data": data}
+
+def save_dataframe_as_csv(df, base_name, output_dirpath, verbose=False) -> str:
     """
     Save pandas dataframe as csv file. Return the file's path.
     """
     if df is None or df.empty:
         return None
-    os.makedirs(output_dir, exist_ok=True)
-    
-    filepath = os.path.join(output_dir, f"{nom}_{type_donnee}.csv")
-    df.to_csv(filepath, index=False, encoding="utf-8")
+    os.makedirs(output_dirpath, exist_ok=True)
+    filepath = os.path.join(output_dirpath, f"{base_name}.csv")
+    _ = df.to_csv(filepath, index=False, encoding="utf-8")
 
-    filesize = os.path.getsize(filepath) / 1024
+    filesize_in_kb = os.path.getsize(filepath) / 1024
     if verbose:
-        print(f">>> Sauvegardé : {filepath} ({len(df)} lignes, {filesize:.0f} Ko)")
+        print(f">>> Sauvegardé : {filepath} ({len(df)} lignes, {filesize_in_kb:.0f} Kb)")
     return filepath
